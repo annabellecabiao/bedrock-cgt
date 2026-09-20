f90d2719de17b9d9b9fc23a3e605ec0d48835005
Annabelle <annabellecabiao@gmail.com>
2026-09-20 23:28:52 +0200

    Add frontend/src

--- frontend/src/layouts/MainLayout.tsx
+++ frontend/src/layouts/MainLayout.tsx
@@ -1,5 +1,5 @@
 import { Outlet } from "react-router-dom";
-import Navbar from "../components/Navbar/Navbar.tsx";
+import Navbar from "../components/Navbar/Navbar";
 import Footer from "../components/Footer/Footer";
 
 export default function MainLayout() {
