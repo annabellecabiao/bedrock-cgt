abcce67b953f92217b5561c1cdce17d30523a648
Annabelle <annabellecabiao@gmail.com>
2026-09-20 22:54:03 +0200

    Add frontend/src

--- frontend/src/pages/Home/Home.tsx
+++ frontend/src/pages/Home/Home.tsx
@@ -1,12 +1,4 @@
-function Navbar() {
-  return (
-    <nav className="w-full bg-bedrock-navy px-6 py-4">
-      <a href="/" className="font-serif text-xl text-white">
-        Bedrock Consumer Goods Trading
-      </a>
-    </nav>
-  );
-}
+import Navbar from "../../components/Navbar/Navbar";
 
 export default function Home() {
   return (
